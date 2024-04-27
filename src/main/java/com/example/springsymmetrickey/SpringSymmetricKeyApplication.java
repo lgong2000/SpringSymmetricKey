51a5e0e850d4260e235f51bbbec0a6e2671bcfc7
lgong2000 <lgong2000@gmail.com>
2024-04-27 12:33:24 -0500

    Initial Commit

---
+++ src/main/java/com/example/springsymmetrickey/SpringSymmetricKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsymmetrickey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSymmetricKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSymmetricKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsymmetrickey/SpringSymmetricKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsymmetrickey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSymmetricKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
